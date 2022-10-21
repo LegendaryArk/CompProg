db6edae40f6b1c7757a4dca462797a878d033fc1
Noah <40373981+LegendaryArk@users.noreply.github.com>
2022-10-21 09:47:20 -0400

    Smile with Similes
    https://dmoj.ca/problem/ccc04j3

---
+++ Problems/ccc04j3/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	vector<string> adjs, nouns;
+	for (int i = 0; i < n; i++) {
+		string adj;
+		cin >> adj;
+		adjs.push_back(adj);
+	}
+	for (int i = 0; i < m; i++) {
+		string noun;
+		cin >> noun;
+		nouns.push_back(noun);
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << adjs[i] << " as " << nouns[j] << "\n";
+		}
+	}
+}
