ec6c6317117e530fa7ea70a1946454fa4099c2f5
Noah <40373981+LegendaryArk@users.noreply.github.com>
2022-10-21 10:04:20 -0400

    Anagram Checker
    https://dmoj.ca/problem/ccc07j4

---
+++ Problems/ccc07j4/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+
+	cout << s1 << "\t" << s2 << "\n";
+
+	 s1.erase(remove(s1.begin(), s1.end(), ' '), s1.end());
+	 s2.erase(remove(s2.begin(), s2.end(), ' '), s2.end());
+
+	cout << s1 << "\t" << s2 << "\n";
+
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end());
+	
+	cout << s1 << "\t" << s2 << "\n";
+
+	for (int i = 0; i < s1.length(); i++) {
+		if (s1[i] != s2[i]) {
+			cout << "Is not an anagram.\n";
+			return 0;
+		}
+	}
+	cout << "Is an anagram.\n";
+}
